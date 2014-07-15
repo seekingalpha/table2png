f5f3b82f46c5adc9687bc578c3c2ebe191e578fd
Adam Rubin <arubin@seekingalpha.com>
2014-07-15 21:45:04 +0300

    now you can pass list of css files path

--- lib/table2png/converter.rb
+++ lib/table2png/converter.rb
@@ -1,11 +1,11 @@
 module Table2PNG
-  attr_accessor :css_files
 
   IMGKit.configure do |config|
     config.default_options = { quality: 100, "disable-javascript" => true }
   end
 
-  Converter = Struct.new(:html, :new_width) do
+  Converter = Struct.new(:html, :new_width, :css_files) do
+
     def save path
       process
       png.save path
@@ -19,23 +19,24 @@ module Table2PNG
       png.height
     end
 
-    def process
+    def process css_files = nil
+      raise 'Please pass list of css path i.e [\'/public/file.css\',\'/public/file2.css\']' if css_files && !css_files.is_a?(Array)
+      @css_files = css_files || ['public/table.css']
       png.trim!
     end
 
     private
     def png
-      css_files = @css_files || ['public/table.css']
-      @png ||= ChunkyPNG::Image.from_blob png_from_table(css_files)
+      @png ||= ChunkyPNG::Image.from_blob png_from_table
     end
 
 
-    def png_from_table css_files
+    def png_from_table
       return @png_from_table if defined?(@png_from_table)
 
       image_from_table = IMGKit.new(html)
 
-      css_files.each do |file_name|
+      @css_files.each do |file_name|
         image_from_table.stylesheets << "#{Dir.pwd}/#{file_name}"
       end
 
--- spec/converter_spec.rb
+++ spec/converter_spec.rb
@@ -4,10 +4,11 @@ include Table2PNG
 
 describe Converter do
 
-  subject(:png) { Converter.new(table).process }
-  
+  subject(:png) { Converter.new(table).process style}
+
   describe '#process' do
     context 'given a small table' do
+      let(:style) { nil }
       let(:table) { File.read(Dir.pwd + "/spec/fixtures/small_table.html") }
       let(:result) { File.read(Dir.pwd + "/spec/fixtures/small.png") }
 
@@ -19,6 +20,7 @@ describe Converter do
     end
 
     context 'given a big table' do
+      let(:style) { nil }
       let(:table) { File.read(Dir.pwd + "/spec/fixtures/big_table.html") }
       let(:result) { File.read(Dir.pwd + "/spec/fixtures/big.png") }
       
@@ -28,5 +30,24 @@ describe Converter do
         expect(png.height).to eq(414) 
       end
     end
+
+    context 'given a small table with other style file' do
+      let(:style) { ['public/other_style.css'] }
+      let(:table) { File.read(Dir.pwd + "/spec/fixtures/small_table.html") }
+      let(:result) { File.read(Dir.pwd + "/spec/fixtures/other_style_small.png")}
+
+      it 'should have new design' do
+        expect(png.to_blob).to eq(result)
+      end
+    end
+
+    context 'given a small table with wrong style file' do
+      it 'should raises' do
+        expect{Converter.new(File.read(Dir.pwd + "/spec/fixtures/small_table.html")).process('public/style.css')}.to raise_error(
+          RuntimeError,
+          'Please pass list of css path i.e [\'/public/file.css\',\'/public/file2.css\']')
+      end
+    end
   end
+
 end
