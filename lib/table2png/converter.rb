491e76d1ae484a038c3c0e0c178e6c7ee79ae1bf
Guy Issler <gissler@seekingalpha.com>
2014-07-15 10:39:51 +0300

    change in png_from_table method

--- lib/table2png/converter.rb
+++ lib/table2png/converter.rb
@@ -25,12 +25,16 @@ module Table2PNG
     def png
       @png ||= ChunkyPNG::Image.from_blob png_from_table
     end
-    
-    def png_from_table 
+
+    def png_from_table sources
       return @png_from_table if defined?(@png_from_table)
 
       image_from_table = IMGKit.new(html)
-      image_from_table.stylesheets << "#{Dir.pwd}/table.css"
+
+      css_files = Synthesis::AssetPackage.sources_from_targets("stylesheets", sources)
+      css_files.each do |file_name|
+        image_from_table.stylesheets << "#{Dir.pwd}/public/stylesheets/" + file_name + ".css"
+      end
 
       @png_from_table = image_from_table.to_img(:png)
     end
