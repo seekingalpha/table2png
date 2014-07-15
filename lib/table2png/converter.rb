e8a409116fa4fea3356adc69846c48d2946156fe
Guy Issler <gissler@seekingalpha.com>
2014-07-15 11:07:43 +0300

    update method...

--- lib/table2png/converter.rb
+++ lib/table2png/converter.rb
@@ -23,15 +23,17 @@ module Table2PNG
 
     private
     def png
-      @png ||= ChunkyPNG::Image.from_blob png_from_table
+      sources = ['article','performance_layout','performance_layout_b']
+      css_files = Synthesis::AssetPackage.sources_from_targets("stylesheets", sources)
+      @png ||= ChunkyPNG::Image.from_blob png_from_table css_files
     end
 
-    def png_from_table sources
+
+    def png_from_table css_files
       return @png_from_table if defined?(@png_from_table)
 
       image_from_table = IMGKit.new(html)
 
-      css_files = Synthesis::AssetPackage.sources_from_targets("stylesheets", sources)
       css_files.each do |file_name|
         image_from_table.stylesheets << "#{Dir.pwd}/public/stylesheets/" + file_name + ".css"
       end
