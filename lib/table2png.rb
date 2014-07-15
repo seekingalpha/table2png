da0869668edb2fd6be2c0a7d4c40eec1dfa31e85
Guy Issler <gissler@seekingalpha.com>
2014-07-15 12:37:14 +0300

    removed unneeded init code

--- lib/table2png.rb
+++ lib/table2png.rb
@@ -2,11 +2,6 @@ module Table2PNG
   autoload :Converter, 'table2png/converter'
 
   def convert(html, width=nil)
-    IMGKit.configure do |config|
-      binary_path = '/usr/local/bin/wkhtmltoimage'
-      config.wkhtmltoimage = File.join("", binary_path)
-    end
-
     Converter.new(html, width).process
   end
 end
